bff19df142b59449108de73bd81fa006dc3b7cf0
mildzan@gmail.com <mildzan@gmail.com>
2020-04-23 13:46:37 +0200

    4, 5

--- Part1.js
+++ Part1.js
@@ -1,16 +1,16 @@
-// Function that returns the sum of all numbers
+// 1.Function that returns the sum of all numbers
 
-const sum = (...listOfNumbers) => {
+const sumAll = (...listOfNumbers) => {
   let result = 0;
   listOfNumbers.forEach((el) => (result += el));
-  console.log(result);
+  console.log("sum of all numbers: " + result);
 };
 
-sum(1, 2, 3, 4, 5); // 15
-sum(1, 2, 3, 4, 6, 7, 8, 9, 10); // 50
-//*******************************************************//
+sumAll(1, 2, 3, 4, 5); // 15
+sumAll(1, 2, 3, 4, 6, 7, 8, 9, 10); // 50
+//****************************************************************//
 
-// To given rectangular matrix of characters add a border of astrisk(*)
+// 2.To given rectangular matrix of characters add a border of astrisk(*)
 
 const addBorder = (picture) => {
   const wall = "*".repeat(picture[0].length + 2);
@@ -22,20 +22,47 @@ const addBorder = (picture) => {
   console.log(border);
 };
 
-const picture = ["abc", "def"];
-const picture2 = ["abcd", "efgh"];
-addBorder(picture); // [ '*****', '*abc*', '*def*', '*****' ]
-addBorder(picture2); // [ '******', '*abcd*', '*efgh*', '******' ]
-//*******************************************************//
+addBorder(["abc", "def"]); // [ '*****', '*abc*', '*def*', '*****' ]
+addBorder(["abcd", "efgh"]); // [ '******', '*abcd*', '*efgh*', '******' ]
+//****************************************************************//
 
-// For given integer return the sum of its digits.
+// 3.For given integer return the sum of its digits.
 
 const sumOfDigits = (num) => {
   const arr = [...num.toString()];
   const sum = arr.reduce((total, el) => {
     return total + parseInt(el);
   }, 0);
-  console.log(sum);
+  console.log("sum: " + sum);
 };
 sumOfDigits(12); // 3
 sumOfDigits(235); // 10
+
+//****************************************************************//
+
+// 4.For given array of integers, find the pair of adjacent element that has the largest product.
+
+const largestProduct = (arr) => {
+  let max = 0;
+  for (i = 0; i < arr.length - 1; i++) {
+    const product = arr[i] * arr[i + 1];
+    max = product > max ? product : max;
+  }
+  console.log("max: " + max);
+};
+
+largestProduct([3, 6, -2, -5, 7, 3]); // 21
+//****************************************************************//
+
+// 5.For a given array of strings, return another array containing all of its longest strings.
+
+const longestStrings = (arr) => {
+  let longest = 0;
+  arr.forEach((el) => {
+    longest = el.length > longest ? el.length : longest;
+  });
+  const result = arr.filter((el) => el.length === longest);
+  console.log(result);
+};
+
+longestStrings(["bad", "ab", "lsd", "gag", "a", "dr"]); // [ 'bad', 'lsd', 'gag' ]
