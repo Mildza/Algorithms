590ede066db4990084de9f8829e0d48326417066
mildzan@gmail.com <mildzan@gmail.com>
2020-04-26 13:21:36 +0200

    3 and 4

--- Part2.js
+++ Part2.js
@@ -56,3 +56,43 @@ const sumProduct = (arr) => {
 sumProduct([1, 2, 3, 4, 5, 6, 7, 8]); // 186
 // [1, 2, 3, 4, 5, 6, 7, 8] => [3, 7, 11, 15] => [21, 165] => [186]
 //****************************************************************//
+
+// 3. For given array of integers, find the maximal absolute differnce between any two of its adjacent elements.
+
+const maxDifference = (arr) => {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    max =
+      Math.abs(arr[i] - arr[i + 1]) > max ? Math.abs(arr[i] - arr[i + 1]) : max;
+  }
+  console.log("max: " + max);
+};
+
+maxDifference([2, 4, 1, 0]); // 3
+maxDifference([7, -3, 0, 8]); // 10
+
+//****************************************************************//
+
+// 4. For given an array of integers, replace all the occurrences of the element to replace with substituion element.
+
+const arrayReplace = (arr, el, sub) => {
+  // String version
+  const str = arr.toString().replace(new RegExp(el, "g"), sub);
+  console.log(str.split(",").map((el) => Number(el)));
+  arr2 = [...arr];
+  // Array version 1
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === el) {
+      arr.splice(i, 1, sub);
+    }
+  }
+  console.log(arr);
+  // Array version 2
+  arr2.forEach((item, index) => {
+    if (item === el) {
+      arr2[index] = sub;
+    }
+  });
+  console.log(arr2);
+};
+arrayReplace([1, 2, 1], 1, 3); // elToReplace=1, subEl=3 => [3, 2, 3]
